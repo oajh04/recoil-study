f90ff8c2a08a09f3318b3fc58ffa7088fbf99f54
oajh04 <cloudtubekr@naver.com>
2021-05-08 22:24:01 +0900

    app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Clock from './Components/Clock/Clock';
+import Test from './Components/Test';
+
+function App() {
+  return (
+    <>  
+      <Test/>
+    </>
+  );
+}
+
+export default App;
